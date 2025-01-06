cc607e7acafa14678e4fe4ac55f8c1cf7729ddc2
Tangheab <tongheabtang@gmail.com>
2025-01-06 23:33:05 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('revealConfessionBtn').addEventListener('click', function() {
+    document.getElementById('confessionText').style.display = 'block';
+    document.getElementById('revealConfessionBtn').style.display = 'none';
+});
+
